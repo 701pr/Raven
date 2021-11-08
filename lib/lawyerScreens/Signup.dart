4a2d2c1da2368164a60573373a31c1cb46e703dc
701pr <60639343+701pr@users.noreply.github.com>
2021-11-08 19:21:52 +0530

    Add files via upload

---
+++ lib/lawyerScreens/Signup.dart
@@ -0,0 +1,254 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+import 'package:pocketklawbook/Lawpage.dart';
+import 'package:pocketklawbook/model/user_model.dart';
+
+class Lsignup extends StatefulWidget {
+  const Lsignup({Key? key}) : super(key: key);
+
+  @override
+  _LsignupState createState() => _LsignupState();
+}
+
+class _LsignupState extends State<Lsignup> {
+  final _auth = FirebaseAuth.instance;
+  final _formKey = GlobalKey<FormState>();
+  final firstNameEditingController = TextEditingController();
+  final lastNameEditingController = TextEditingController();
+  final emailEditingController = TextEditingController();
+  final passwordEditingController = TextEditingController();
+  final confirmPasswordEditingController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    final signUpButton = Material(
+      elevation: 50.0,
+      borderRadius: BorderRadius.circular(40.0),
+      color: Colors.red,
+      child: MaterialButton(
+        padding: EdgeInsets.symmetric(
+          vertical: 15.0,
+        ),
+        minWidth: MediaQuery.of(context).size.width,
+        onPressed: () {
+          signUp(emailEditingController.text, passwordEditingController.text);
+        },
+        child: Text(
+          'SignUp',
+          style: TextStyle(
+              color: Colors.white, fontSize: 20.0, fontWeight: FontWeight.bold),
+          textAlign: TextAlign.center,
+        ),
+      ),
+    );
+    final firstNameField = TextFormField(
+      autofocus: true,
+      controller: firstNameEditingController,
+      keyboardType: TextInputType.name,
+      validator: (value) {
+        RegExp regex = new RegExp(r'^.{3,}$');
+        if (value!.isEmpty) {
+          return ("First name cannot be empty");
+        }
+        if (!regex.hasMatch(value)) {
+          return ("Enter Valid Name(Min. 3 Character)");
+        }
+      },
+      onSaved: (value) => firstNameEditingController.text = value!,
+      textInputAction: TextInputAction.next,
+      decoration: InputDecoration(
+        prefixIcon: Icon(Icons.person),
+        labelText: 'First Name',
+        hintText: 'Enter First Name',
+        contentPadding: EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
+        border: OutlineInputBorder(borderRadius: BorderRadius.circular(10.0)),
+      ),
+    );
+    final lastNameField = TextFormField(
+      autofocus: true,
+      controller: lastNameEditingController,
+      keyboardType: TextInputType.name,
+      validator: (value) {
+        if (value!.isEmpty) {
+          return ("First name cannot be empty");
+        }
+        return null;
+      },
+      onSaved: (value) => lastNameEditingController.text = value!,
+      textInputAction: TextInputAction.next,
+      decoration: InputDecoration(
+        prefixIcon: Icon(Icons.person),
+        labelText: 'Last Name',
+        hintText: 'Enter Last Name',
+        contentPadding: EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
+        border: OutlineInputBorder(borderRadius: BorderRadius.circular(10.0)),
+      ),
+    );
+    final emailField = TextFormField(
+      autofocus: true,
+      controller: emailEditingController,
+      keyboardType: TextInputType.emailAddress,
+      validator: (value) {
+        if (value!.isEmpty) {
+          return 'Please enter your email';
+        }
+        if (!RegExp("^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]+.[a-z]").hasMatch(value)) {
+          return ("Please Enter a valid email");
+        }
+        return null;
+      },
+      onSaved: (value) => emailEditingController.text = value!,
+      textInputAction: TextInputAction.next,
+      decoration: InputDecoration(
+        prefixIcon: Icon(Icons.email),
+        labelText: 'Email',
+        hintText: 'Enter Email Address',
+        contentPadding: EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
+        border: OutlineInputBorder(borderRadius: BorderRadius.circular(10.0)),
+      ),
+    );
+    final passwordField = TextFormField(
+      autofocus: true,
+      obscureText: true,
+      controller: passwordEditingController,
+      validator: (value) {
+        RegExp regex = new RegExp(r'^.{8,}$');
+        if (value!.isEmpty) {
+          return ("Password is required for Signup");
+        }
+        if (!regex.hasMatch(value)) {
+          return ("Enter Valid Password(Min. 8 Character)");
+        }
+      },
+      onSaved: (value) => passwordEditingController.text = value!,
+      textInputAction: TextInputAction.next,
+      decoration: InputDecoration(
+        prefixIcon: Icon(Icons.vpn_key),
+        labelText: 'Password',
+        hintText: 'Type a Password',
+        contentPadding: EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
+        border: OutlineInputBorder(borderRadius: BorderRadius.circular(10.0)),
+      ),
+    );
+    final confirmPasswordField = TextFormField(
+      autofocus: true,
+      obscureText: true,
+      controller: confirmPasswordEditingController,
+      validator: (value) {
+        if (confirmPasswordEditingController.text !=
+            passwordEditingController.text) {
+          return ("Password doesn't match");
+        }
+        return null;
+      },
+      onSaved: (value) => confirmPasswordEditingController.text = value!,
+      textInputAction: TextInputAction.done,
+      decoration: InputDecoration(
+        prefixIcon: Icon(Icons.vpn_key),
+        labelText: 'Confirm Password',
+        hintText: 'Re-Type the password',
+        contentPadding: EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
+        border: OutlineInputBorder(borderRadius: BorderRadius.circular(10.0)),
+      ),
+    );
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0.0,
+        leading: IconButton(
+          icon: Icon(Icons.arrow_back),
+          color: Colors.black,
+          onPressed: () {
+            Navigator.pop(context);
+          },
+        ),
+      ),
+      backgroundColor: Colors.white,
+      body: Center(
+        child: SingleChildScrollView(
+          child: Container(
+            color: Colors.white,
+            child: Padding(
+              padding: const EdgeInsets.all(36.0),
+              child: Form(
+                key: _formKey,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: <Widget>[
+                    SizedBox(
+                      height: 155.0,
+                      child: Image.asset(
+                        "assets/raven.png",
+                        fit: BoxFit.contain,
+                      ),
+                    ),
+                    firstNameField,
+                    SizedBox(height: 35.0),
+                    lastNameField,
+                    SizedBox(height: 35.0),
+                    emailField,
+                    SizedBox(height: 35.0),
+                    passwordField,
+                    SizedBox(height: 35.0),
+                    confirmPasswordField,
+                    SizedBox(height: 35.0),
+                    signUpButton,
+                    SizedBox(
+                      height: 15,
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  void signUp(String email, String password) async {
+    if (_formKey.currentState!.validate()) {
+      await _auth
+          .createUserWithEmailAndPassword(email: email, password: password)
+          .then((value) => {postDetailsToFirestore()})
+          .catchError((e) {
+        Fluttertoast.showToast(
+            msg: e!.message,
+            toastLength: Toast.LENGTH_SHORT,
+            gravity: ToastGravity.BOTTOM,
+            backgroundColor: Colors.red,
+            textColor: Colors.white,
+            fontSize: 16.0);
+      });
+    }
+  }
+
+  postDetailsToFirestore() async {
+    // calling our firestore
+    // calling our user model
+    // sedning these values
+
+    FirebaseFirestore firebaseFirestore = FirebaseFirestore.instance;
+    User? lawyer = _auth.currentUser;
+
+    UserModel userModel = UserModel();
+
+    // writing all the values
+    userModel.email = lawyer!.email;
+    userModel.uid = lawyer.uid;
+    userModel.firstName = firstNameEditingController.text;
+    userModel.lastName = lastNameEditingController.text;
+
+    await firebaseFirestore
+        .collection("users")
+        .doc(lawyer.uid)
+        .set(userModel.toMap());
+    Fluttertoast.showToast(msg: "Account created successfully :) ");
+
+    Navigator.pushAndRemoveUntil((context),
+        MaterialPageRoute(builder: (context) => Lawpage()), (route) => false);
+  }
+}
